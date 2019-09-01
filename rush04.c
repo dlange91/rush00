d9c33a38aa5909c1a53aa19ea833020fd4f51e7f
Lange DAVID <ldavid@e2r10p7.42.fr>
2019-09-01 13:37:45 +0200

    first commit

---
+++ rush04.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moudmine <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/31 11:03:12 by moudmine          #+#    #+#             */
+/*   Updated: 2019/08/31 16:50:56 by moudmine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void		ft_putchar(char c);
+
+void		first_line(int colonne)
+{
+	int i;
+
+	i = 0;
+	ft_putchar('A');
+	i++;
+	if (colonne > 1)
+	{
+		while (i + 1 < colonne)
+		{
+			ft_putchar('B');
+			i++;
+		}
+		ft_putchar('C');
+	}
+	ft_putchar('\n');
+}
+
+void		middle(int colonne, int ligne)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 1;
+	while (j + 1 < ligne)
+	{
+		ft_putchar('B');
+		i++;
+		if (colonne > 1)
+		{
+			while (i + 1 < colonne)
+			{
+				write(1, " ", 1);
+				i++;
+			}
+			ft_putchar('B');
+		}
+		ft_putchar('\n');
+		i = 0;
+		j++;
+	}
+}
+
+void		lastline(int colonne)
+{
+	int i;
+
+	i = 0;
+	ft_putchar('C');
+	i++;
+	if (colonne > 1)
+	{
+		while (i + 1 < colonne)
+		{
+			ft_putchar('B');
+			i++;
+		}
+		ft_putchar('A');
+	}
+	ft_putchar('\n');
+}
+
+void		rush(int x, int y)
+{
+	if (x > 0 && y > 0)
+	{
+		first_line(x);
+		if (y >= 2)
+			middle(x, y);
+		if (y >= 3)
+			lastline(x);
+	}
+}
